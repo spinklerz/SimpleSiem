d47c87c384c87e955542edd31fa6ef569b144f33
David_Shonack <dshonack@ucsc.edu>
2025-07-03 20:45:44 -0700

    Simple additions like adding a post to a machine

--- agent/windows_agent.py
+++ agent/windows_agent.py
@@ -2,27 +2,37 @@ import os
 import subprocess
 import json
 import schedule
+import requests
 import time
 import logging
 from datetime import datetime, timedelta
 
 # Defines format of the log
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+def push(payload, ip):
+    try: 
+        r = requests.post(ip, data=payload)
+        r.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"An error occured: {e}")
 
-def scrap_log_and_push(cmd):
+def scrap_log(ip):
         # Currently scrapes logs from windows, the past 25 events from newest to oldest
         # Format is json for easy parsing
         # rd:true just means reverse order to newest to oldest
+        # Feel free to add more logs this is just an example
         cmd = [
         'wevtutil', 'qe', 'System', 
         '/c:25',
         '/rd:true',
         '/f:json' ]
-        output = subprocess.run(cmd, capture_output=True, text)
+        output = subprocess.run(cmd, capture_output=True)
+        push(output, ip)
         return output
 
-def push_logs():
-
+def arg_parse():
+    parser = argparse.ArguementParser(description="A simple siem windows agent, usage: ")
+    parser.add_argument('-i', '--ip-to-forward', help='ip of the machine we want to forward data too', required=True)
     pass
 def main():
     '''
@@ -31,11 +41,11 @@ def main():
         - Get data( Any data we can generate for example network data logs from zeek or  Windows Event Logs )
         - Push the data to the processor who can then organize the data, check for malicous activity etc
     '''
-    # Feel free to adjust push/scrape rate
+    args = arg_parse()    # Feel free to adjust push/scrape rate
     # Time current at 5 minutes per push
     push_rate = 5
     sleep_rate = 290 # 5 * 300 - 10 this is to reduce computational power a bit 
-    output = schedule.every(rate).minutes.do(scrap_log_and_push)
+    output = schedule.every(rate).minutes.do(scrap_log(args.ip_to_forward))
     while True:
         schedule.run_pending()
         time.sleep(sleep_rate)
